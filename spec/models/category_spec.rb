01a90e6464b3a536074fa37d7cfb292758454ed6
Brent Palmer <bmpalmer987@gmail.com>
2015-03-13 01:14:24 -0700

    Installed Shoulda gem and changed tests to use shoulda-matacher.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    cat = Category.new(name:"Horror")
-    cat.save
-    expect(Category.first).to eq(cat)
-  end
+  
+  it { should have_many(:videos)}
 
-  it "has many videos" do
-    Horror = Category.create(name:"Horror")
-    south_park = Video.create(title:"South Park", description:"Funny", category: Horror)
-    futurama = Video.create(title:"Futurama", description:"Funny", category: Horror)
-    expect(Horror.videos).to include(south_park, futurama)
-  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,16 +1,9 @@
 require 'spec_helper'
 
   describe Video do
-    it "saves itself" do
-      video = Video.new(title:"Shindler's List")
-      video.save
-      expect(video.title).to eq("Shindler's List")
-    end
-
-    it "belongs to category" do
-      dramas = Category.create(name:"dramas")
-      monk = Video.create(title:"Monk", description:"Funny", category: dramas)
-      expect(monk.category).to eq(dramas)
-    end
+    
+    it { should belong_to(:category)}
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:description)}
 
 end
