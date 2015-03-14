3ba8350d8945bbdbf12c9f4034a945c9af7d8c29
Brent Palmer <bmpalmer987@gmail.com>
2015-03-14 00:23:04 -0700

    Bug for search feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
 
-  resources :videos
+  resources :videos do
+    collection do
+      get '/search', to: 'videos#search'
+    end
+  end
+
   resources :categories
 
   get 'ui(/:action)', controller: 'ui'
