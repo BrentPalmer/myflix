332e47b158cea86ddf69efe642fee2339393ca8c
Brent Palmer <bmpalmer987@gmail.com>
2015-03-15 13:36:21 -0700

    Added recent_views method in video#index view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Myflix::Application.routes.draw do
+  root 'videos#index'
   get '/home', to: 'videos#index'
 
   resources :videos do
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: "South Park", description: "Four boys in a small town get into shananigains.", small_cover_url: "south_park.jpg", large_cover_url: "")
-Video.create(title: "Family Guy", description: "Family of five have crazy flashbacks.", small_cover_url: "family_guy.jpg", large_cover_url: "")
-Video.create(title: "Futurama", description: "Space comedy.", small_cover_url: "futurama.jpg", large_cover_url: "")
-
 Category.create(name:"Comedies")
 Category.create(name:"Action")
 Category.create(name:"Dramas")
 Category.create(name:"Romance")
 Category.create(name:"Scary")
+
+Video.create(title: "South Park", description: "Four boys in a small town get into shananigains.", small_cover_url: "south_park.jpg", large_cover_url: "", category_id: 1)
+Video.create(title: "Family Guy", description: "Family of five have crazy flashbacks.", small_cover_url: "family_guy.jpg", large_cover_url: "", category_id: 1)
+Video.create(title: "Futurama", description: "Space comedy.", small_cover_url: "futurama.jpg", large_cover_url: "", category_id: 1)
+Video.create(title: "South Park", description: "Four boys in a small town get into shananigains.", small_cover_url: "south_park.jpg", large_cover_url: "", category_id: 1)
+Video.create(title: "Family Guy", description: "Family of five have crazy flashbacks.", small_cover_url: "family_guy.jpg", large_cover_url: "", category_id: 1)
+Video.create(title: "Futurama", description: "Space comedy.", small_cover_url: "futurama.jpg", large_cover_url: "", category_id: 1)
