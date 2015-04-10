8e93d1b1cb9ca4189f8dfaed206c7392973031d3
Brent Palmer <bmpalmer987@gmail.com>
2015-04-09 23:12:40 -0700

    Refactored queue items controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 5}
   validates :name, presence: true
+
+  def normalize_queue_item_positions
+    queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index + 1)
+    end
+  end
 end
